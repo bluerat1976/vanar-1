cc66bb940b5762dea11e5edcd5b56b5bf45f27da
bluerat1976 <osoblivaia76@mail.ru>
2020-02-12 19:48:38 +0200

    in process

---
+++ dino-geim/game.js
@@ -0,0 +1,71 @@
+// data of the map
+// 0- empty 
+// 1 - cactus
+// 2 - meat
+// 9 - rex
+
+const EMPTY = 0
+const CACTUS = 1
+const MEAT = 2
+const REX = 9
+
+
+let data = [9,0,0,0,0,0,0,1,0,0]
+
+function renderMap() {
+    let lend = document.getElementById('land')
+    for(let i =0; i<data.length; i++) {
+
+        let div = document.createElement('div')
+
+        if(data[i] == REX) {
+            div.className = 'dino'
+        
+        } else if(data[i] == CACTUS) {
+            div.className = 'cactus'
+
+        }  else if(data[i] == MEAT) {
+            div.className = 'meat' 
+        
+        }else if(data[i] == EMPTY) {
+            div.className = 'empty'   
+        }
+    
+        lend.appendChild(div)
+    }   
+}
+
+function clearMap() {
+    let lend = document.getElementById('land')
+    lend.innerHTML = ''
+
+    // for + .removeChild()
+}
+
+function move()  {
+    // remove second value(square) from 'data'.
+    data.splice(1,1)
+
+    if(Math.random()<0.3) {
+         data.push(1)
+    } else if(Math.random() < 0.3 ){
+        data.push(2)
+    } else {
+        data.push(0)
+    }
+   
+}
+
+function jump() {
+    alert();
+}
+
+
+
+renderMap()
+
+setInterval(function(){
+    move()
+    clearMap()
+    renderMap()
+}, 1000)
